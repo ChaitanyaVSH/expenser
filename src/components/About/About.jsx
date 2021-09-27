bfb8e77ddfe194a724248bf8959ef089dd9d35a8
Chaitanya <chaitu.vsh@gmail.com>
2021-09-28 00:12:57 +0530

    Added About page placeholder component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,8 +4,13 @@ import styles from "./About.module.css";
 const About = () => {
     return (
         <div className={styles.container}>
-            <h1>About</h1>
-            <h1>About</h1>
+            <h1>Expenser</h1>
+            <h4>All In One Personal Finance Management platform</h4>
+            <hr/>
+            <p>Lorem ipsum dolor sit amet consecteturlorem15 Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iste, id numquam delectus sit inventore? adipisicing elit. Doloremque, labore rem repellat magni aspernatur sunt.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, labore rem repellat magni aspernatur sunt. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate dolorem quae fuga! Praesentium, eum veniam iusto quisquam nemo earum ullam sapiente dolores excepturi! Aliquam autem ipsa nobis possimus consequatur. Numquam, voluptatibus! Magni quos ad libero, dolor similique minima hic ex excepturi quidem, modi numquam nemo dolorem illum placeat eum debitis!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, labore rem repellat magni aspernatur sunt. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate dolorem quae fuga! Praesentium, Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, alias? Debitis sit maxime qui, facilis praesentium, asperiores facere porro inventore illo id fugiat. Enim reiciendis ex, culpa animi delectus similique quibusdam eaque repudiandae natus, minus expedita amet. Incidunt, maxime quo.eum veniam iusto quisquam nemo earum ullam sapiente dolores excepturi! Aliquam autem ipsa nobis possimus consequatur. Numquam, voluptatibus! Magni quos ad libero, dolor similique minima hic ex excepturi quidem, modi numquam nemo dolorem illum placeat eum debitis!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium facilis deleniti quod excepturi quis maiores quae, error hic. Optio placeat facere ad pariatur soluta, neque ullam quis illo minus quaerat officiis dolores sequi veritatis sed alias a dignissimos autem odio. Fuga expedita sapiente iusto dolores. Tempora delectus mollitia est voluptatum ipsam in repellat cum fugiat id numquam iusto vero dolores voluptatibus illo enim eaque libero nostrum corrupti accusamus, aperiam recusandae eum tempore quos ipsa! Explicabo voluptatum, blanditiis cumque ipsum quam neque quaerat fugiat, ad vitae expedita, minima ullam quis provident rem! Facilis sit vero minus laudantium vel debitis recusandae, magni doloremque quasi quos deserunt molestiae quae blanditiis rem totam laborum quisquam voluptatum ratione modi dolorum dicta atque in doloribus dolores! Aliquid mollitia dignissimos ad libero delectus eligendi quo quos et aut iure velit incidunt voluptatibus temporibus architecto distinctio, aperiam eos deleniti facere numquam! Atque laboriosam provident excepturi beatae? Quae, a.</p>
         </div>
     )
 }
