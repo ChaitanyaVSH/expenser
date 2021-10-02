4e9f528ba3ef9f54c5459f87c415bfe37d1b151c
Chaitanya <chaitu.vsh@gmail.com>
2021-10-02 23:29:32 +0530

    [UTILS] Implemented local storage utilities to get and set the data items in the local storage

---
+++ src/utils/LocalStorage/localStorageUtils.js
@@ -0,0 +1,32 @@
+/**
+ * Helper method to get items from the local storage.
+ * @param {0} key, key of the local storage item.
+ * @param {1} parse, boolean which determines whether to parse the response.
+ * @returns response from the local storage.
+ */
+export const getItem = (key, parse = false) => {
+    return parse ? JSON.parse(localStorage.getItem(key)) : localStorage.getItem(key);
+};
+
+/**
+ * Helper method to store items in the local storage.
+ * @param {0} key, key of the local storage item.
+ * @param {1} value, value against the key to store in the local storage.
+ * @param {2} stringify, boolean which determines whether to stringify the data before storing in the local storage.
+ * @returns true if able to store the {key: value} in local storage, else returns false if failed to store the data.
+ */
+export const setItem = (key, value, stringify = false) => {
+    try {
+        if (stringify) {
+            localStorage.setItem(key, JSON.stringify(value));
+            return true;
+        }
+        else {
+            localStorage.setItem(key, value);
+            return true;
+        }
+    } catch (error) {
+        console.error(error);
+        return false
+    }
+};
