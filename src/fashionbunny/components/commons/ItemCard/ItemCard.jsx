64fedbbd39bd572be344f49cac7cc80c6b2cae44
Chaitanya <chaitu.vsh@gmail.com>
2024-07-07 12:10:28 +0530

    [FashionBunny]: Added color, size and addition of same item to cart with different parameters feature

--- src/fashionbunny/components/commons/ItemCard/ItemCard.jsx
+++ src/fashionbunny/components/commons/ItemCard/ItemCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import styles from "./ItemCard.module.css";
 
 // Redux
@@ -10,9 +10,21 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { ADD_ITEM_TO_CART, TRY_AT_HOME_CART } from '../../../../redux/actions/fashionbunny/types';
 
-const ItemCard = ({id, image, title, desc, price }) => {
+const ItemCard = ({id, image, title, desc, price, supportedColors, supportedSizes }) => {
 
   const dispatch = useDispatch();
+
+  const [selectionState, setSelectionState] = useState({
+    selectedColor: supportedColors[0],
+    selectedSize: supportedSizes[0]
+  });
+
+  const clickHandler = (event) => {
+    setSelectionState({
+      ...selectionState,
+      [event.target.name]: event.target.value
+    })
+  };
 
   const addToCart = (cartType, cartItem) => {
     console.log("Adding item to cart: shop");
@@ -33,8 +45,46 @@ const ItemCard = ({id, image, title, desc, price }) => {
         <Card.Text>
             Price:: {price}
         </Card.Text>
+        {supportedColors.map((color, idx) =>
+        <Button
+          name="selectedColor"
+          key={idx}
+          style={{
+            backgroundColor: `${color}`,
+            width: "25px",
+            height: "25px",
+            borderRadius: "50%",
+            marginBottom: "10px",
+            border: `${selectionState.selectedColor === color ? "2px solid #000" : "none"}`
+          }}
+          value={color}
+          onClick={(event) => clickHandler(event)}>
+        </Button>)}
+        <br/>
+        {supportedSizes.map((size, idx) =>
+        <Button
+          name="selectedSize"
+          key={idx}
+          variant="light"
+          style={{
+            textAlign: "center",
+            marginBottom: "10px",
+            border: `${selectionState.selectedSize === size ? "2px solid #000" : "none"}`
+          }}
+          value={size}
+          onClick={(event) => clickHandler(event)}
+        >
+          {size}
+        </Button>)}
+        <br/>
         <Button variant="success" onClick={() => addToCart(TRY_AT_HOME_CART, {
-          id, image, title, desc, price
+          id,
+          image,
+          title,
+          desc,
+          price,
+          selectedColor: selectionState.selectedColor,
+          selectedSize: selectionState.selectedSize
         })}>Try at Home</Button>
         <Button variant="light">Buy now</Button>
       </Card.Body>
--- src/fashionbunny/mockdata/items.js
+++ src/fashionbunny/mockdata/items.js
@@ -4,83 +4,107 @@ export const items = [
         image: "https://rukminim2.flixcart.com/image/850/1000/xif0q/shirt/x/u/h/m-st2-vebnor-original-imagpw7w3ha4tv2h.jpeg?q=90&crop=false",
         title: "Rare rabbit shirt",
         desc: "this is very good apparel that matches your style",
-        price: 100
+        price: 100,
+        supportedColors: ["#FFEF0F", "#0FFF04", "#FF0000", "#CCCFFF"],
+        supportedSizes: ["S", "XL", "XXL"]
     },
     {
         id: 2,
         image: "https://www.redwolf.in/image/cache/catalog/sweatshirts/redwolf-basics-plain-black-hoodie-t-shirt-india-600x800.jpg?m=1704439588",
         title: "One 8 hoodie",
         desc: "this is very good apparel that matches your style",
-        price: 400
+        price: 400,
+        supportedColors: ["#FFEF0F", "#0FFF04", "#FF0000", "#CCCFFF"],
+        supportedSizes: ["S", "XL", "XXL"]
     },
     {
         id: 3,
         image: "https://t3.ftcdn.net/jpg/04/83/25/50/360_F_483255019_m1r1ujM8EOkr8PamCHF85tQ0rHG3Fiqz.jpg",
         title: "Cargo Jeans",
         desc: "this is very good apparel that matches your style",
-        price: 1000
+        price: 1000,
+        supportedColors: ["#FFEF0F", "#0FFF04", "#0D0000", "#CCCFFF"],
+        supportedSizes: ["S", "XL", "XXL"]
     },
     {
         id: 5,
         image: "https://5.imimg.com/data5/RS/YS/RU/SELLER-80232528/mens-formal-pants.jpg",
         title: "Formal pant",
         desc: "this is very good apparel that matches your style",
-        price: 100
+        price: 100,
+        supportedColors: ["#FFEF0F", "#0FFF04", "#FF0000", "#CCCFFF"],
+        supportedSizes: ["S", "XL", "XXL"]
     },
     {
         id: 7,
-        image: "https://www.redwolf.in/image/cache/catalog/sweatshirts/redwolf-basics-plain-black-hoodie-t-shirt-india-600x800.jpg?m=1704439588",
-        title: "One 8 hoodie",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0-aqw0eCxTN_jXpSfUssCvpT84hsn0ni0og&s",
+        title: "Classic T-Shirt",
         desc: "this is very good apparel that matches your style",
-        price: 400
+        price: 400,
+        supportedColors: ["#FFEF0F", "#0FFF04", "#FF0000", "#CCCFFF"],
+        supportedSizes: ["S", "XL", "XXL"]
     },
     {
         id: 8,
-        image: "https://t3.ftcdn.net/jpg/04/83/25/50/360_F_483255019_m1r1ujM8EOkr8PamCHF85tQ0rHG3Fiqz.jpg",
+        image: "https://5.imimg.com/data5/RS/YS/RU/SELLER-80232528/mens-formal-pants.jpg",
         title: "Cargo Jeans",
         desc: "this is very good apparel that matches your style",
-        price: 1000
+        price: 1000,
+        supportedColors: ["#FFEF0F", "#0FFF04", "#0D0000", "#CCCFFF"],
+        supportedSizes: ["S", "XL", "XXL"]
     },
     {
         id: 9,
         image: "https://5.imimg.com/data5/RS/YS/RU/SELLER-80232528/mens-formal-pants.jpg",
         title: "Formal pant",
         desc: "this is very good apparel that matches your style",
-        price: 100
+        price: 100,
+        supportedColors: ["#FFEF0F", "#0FFF04", "#0D0000", "#CCCFFF"],
+        supportedSizes: ["S", "XL", "XXL"]
     },
     {
         id: 10,
         image: "https://5.imimg.com/data5/RS/YS/RU/SELLER-80232528/mens-formal-pants.jpg",
         title: "Formal pant",
         desc: "this is very good apparel that matches your style",
-        price: 100
+        price: 100,
+        supportedColors: ["#FFEF0F", "#0FFF04", "#FF0000", "#CCCFFF"],
+        supportedSizes: ["S", "XL", "XXL"]
     },
     {
         id: 11,
         image: "https://5.imimg.com/data5/RS/YS/RU/SELLER-80232528/mens-formal-pants.jpg",
         title: "HRX shirt",
         desc: "this is very good apparel that matches your style",
-        price: 200
+        price: 200,
+        supportedColors: ["#FFEF0F", "#0FFF04", "#0D0000", "#CCCFFF"],
+        supportedSizes: ["S", "XL", "XXL"]
     },
     {
         id: 12,
         image: "https://www.redwolf.in/image/cache/catalog/sweatshirts/redwolf-basics-plain-black-hoodie-t-shirt-india-600x800.jpg?m=1704439588",
         title: "One 8 hoodie",
         desc: "this is very good apparel that matches your style",
-        price: 400
+        price: 400,
+        supportedColors: ["#FFEF0F", "#0FFF04", "#0D0000", "#CCCFFF"],
+        supportedSizes: ["S", "XL", "XXL"]
     },
     {
         id: 13,
         image: "https://t3.ftcdn.net/jpg/04/83/25/50/360_F_483255019_m1r1ujM8EOkr8PamCHF85tQ0rHG3Fiqz.jpg",
         title: "Cargo Jeans",
         desc: "this is very good apparel that matches your style",
-        price: 1000
+        price: 1000,
+        supportedColors: ["#FFEF0F", "#0FFF04", "#0D0000", "#CCCFFF"],
+        supportedSizes: ["S", "XL", "XXL"]
     },
     {
         id: 14,
         image: "https://5.imimg.com/data5/RS/YS/RU/SELLER-80232528/mens-formal-pants.jpg",
         title: "Formal pant",
         desc: "this is very good apparel that matches your style",
-        price: 100
+        price: 100,
+        supportedColors: ["#FFEF0F", "#0FFF04", "#0D0000", "#CCCFFF"],
+        supportedSizes: ["S", "XL", "XXL"]
     },
 ];
